b9d5df57b98b3d9c59df00d48555c52eb47f2dd8
Oyuki Briones <oyukibriones@gmail.com>
2012-12-05 23:59:30 -0600

    Agregando la vista de productos

---
+++ app/formas/productos/Filtro.java
@@ -0,0 +1,11 @@
+package formas.productos;
+
+import play.data.validation.Constraints.Required;
+
+
+public class Filtro {
+	public String clav_prod;
+	public String nom_prod;
+	public String prec_prod;
+
+}
